f74c8bb132c475530a4d25c8f0addcc17956e00d
rmoraes <rmoraes@mprs.mp.br>
2024-05-11 23:10:55 -0300

    iniciando

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+﻿import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+
+const Navbar = () => {
+    return (
+        <nav className='container'>
+            <img src={logo} alt="" className='logo' />
+            <ul>
+                <li>Home</li>
+                <li>Program</li>
+                <li>About</li>
+                <li>Campus</li>
+                <li>Testemonial</li>
+                <li><button className='btn'>Cotact</button></li>
+
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
