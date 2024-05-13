55f9dd268cae0d09cf18ae9af4183ac120e7b55c
rmoraes <rmoraes@mprs.mp.br>
2024-05-13 14:17:42 -0300

    adiciona hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar />
+      <Hero />
+
     </div>
+
   )
 
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className='hero'></div>
+    )
+}
+
+export default Hero
