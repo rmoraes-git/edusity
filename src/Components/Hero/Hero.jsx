f1ca6e7e5919680d4bc01c25c65fa8544b289b06
rmoraes <rmoraes@mprs.mp.br>
2024-05-14 15:20:08 -0300

    ajustando hero com msg

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,13 @@ import './Hero.css'
 
 const Hero = () => {
     return (
-        <div className='hero'></div>
+        <div className='hero container'>
+            <div className='hero-text'>
+                <h1>We Ensure education for a better world</h1>
+                <p>Our curriculum is good...</p>
+                <button className='btn'>Explore more</button>
+            </div>
+        </div>
     )
 }
 
